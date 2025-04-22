369599fded2cb0d0e642035db01b58a14eda0a9a
Durionleponey <61945429+Durionleponey@users.noreply.github.com>
2025-04-22 15:29:38 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
 from utils import *
+import unittest
 
 if __name__ == "__main__":
+
+  
 
   print("Voici quelques tests:")
 
   liste1 = [1, 2, 3, 4, 5]
-  moyenne1 = calculer_moyenne(liste1)
-  print(f"La moyenne de {liste1} est : {moyenne1}")
+  liste2= [0,0,1]
+  liste3= []
+  liste4= [0,0,0]
+  liste5= [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+  liste6= [1,-1]
+  
+  #moyenne1 = calculer_moyenne(liste1)
+  #print(f"La moyenne de {liste1} est : {moyenne1}")
 
-  celsius = 25
-  fahrenheit = convertir_temperature(celsius, 'C', 'F')
-  print(f"{celsius}°C est égal à {fahrenheit}°F")
+  self.assertEqual(calculer_moyenne(liste1), 3)
+  self.assertEqual(calculer_moyenne(liste2), 1/3)
+  self.assertEqual(calculer_moyenne(liste3), None)
+  self.assertEqual(calculer_moyenne(liste4), 0)
+  self.assertEqual(calculer_moyenne(liste5), 1)
+  self.assertEqual(calculer_moyenne(liste6), 0)
+  
 
